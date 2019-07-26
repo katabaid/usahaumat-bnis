721fdd01691e89003a73a220287127f6a030a792
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2019-07-27 02:17:23 +0700

    Refactor BNI post function

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const axios = require('axios')
+const R = require('ramda')
+const BniEnc = require('./BniEncryption')
+require('dotenv').config()
+const bniurl = 'https://apibeta.bni-ecollection.com/'
+axios.defaults.baseURL = bniurl
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+const encrypt = data => BniEnc.encrypt(data, process.env.CUSTOMER_ID, process.env.SECRET_KEY)
+
+const decrypt = data => BniEnc.decrypt(data, process.env.CUSTOMER_ID, process.env.SECRET_KEY)
+
+const prepareRequest = data => JSON.stringify({ client_id: process.env.CUSTOMER_ID, data: encrypt(data) })
+
+const post = async req => {
+  try {
+    const res = await axios.post('/', prepareRequest(req))
+    console.log(res)
+    if (res['status'] === '000') console.log(decrypt(res['message']))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = {
+  post
+}
--- server.test.js
+++ server.test.js
@@ -19,29 +19,67 @@ const data = {
   datetime_expired: two_hours.toISOString()
 }
 
-const encrypted_string = BniEnc.encrypt(data, cid, sck)
-const parsed_string = BniEnc.decrypt(encrypted_string, cid, sck)
-
-console.log(encrypted_string)
-console.log(parsed_string)
-console.log('')
-
-const msg = {
+const ten_seconds = new Date(+new Date() + 2 * 10 * 1000)
+const data1 = {
+  type: 'createbilling',
+  trx_amount: 100001,
+  customer_name: 'David',
+  customer_email: 'david@example.com',
+  customer_phone: '08123456781011',
+  description: 'Test Create Billing',
+  trx_id: 'invoice-0001', // this should be unique
+  virtual_account: null,
+  billing_type: 'c',
   client_id: cid,
-  data: encrypted_string
+  datetime_expired: ten_seconds.toISOString()
 }
 
-const msgJSON = JSON.stringify(msg)
+const data2 = {
+  type: 'createbilling',
+  trx_amount: 100001,
+  customer_name: 'David',
+  customer_email: 'david@example.com',
+  customer_phone: '08123456781011',
+  description: 'Test Create Billing',
+  trx_id: 'invoice-test-0001', // this should be unique
+  virtual_account: null,
+  billing_type: 'c',
+  client_id: cid,
+  datetime_expired: ten_seconds.toISOString()
+}
 
-const bniurl = 'https://apibeta.bni-ecollection.com/'
-axios.defaults.baseURL = bniurl
-axios.defaults.headers.post['Content-Type'] = 'application/json'
+// const encrypted_string = BniEnc.encrypt(data, cid, sck)
+// const parsed_string = BniEnc.decrypt(encrypted_string, cid, sck)
 
-axios.post('/', msgJSON)
-  .then(res => console.log(res))
-  .catch(e => console.log(e))
+// console.log(encrypted_string)
+// console.log(parsed_string)
+// console.log('')
+
+// const msg = {
+//   client_id: cid,
+//   data: encrypted_string
+// }
+
+// const msgJSON = JSON.stringify(msg)
+
+// const bniurl = 'https://apibeta.bni-ecollection.com/'
+// axios.defaults.baseURL = bniurl
+// axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+// axios.post('/', msgJSON)
+//   .then(res => console.log(res))
+//   .catch(e => console.log(e))
 
 // it('should connect to BNI server', () => {
 //   expect.assertions(1)
 //   return conn.createBilling
 // })
+
+// const bniresponse = {
+//   status: '000',
+//   data: 'F0UaGRpGR0cgFxUNN1lZDHVOUAdNBlAGCwN8Q04UFhcVSQkQAgp_Bl5YRgF3RUd1CF1ZVjMhDFBKSkYTGxcWGyFHGxQZR0YzZQ'
+// }
+// const bnidata2 = 'GkYVGxpGR0cgFxUNN1lZDHVOUAdNBlAGCwN8Q04UFhcVSgkQAgp_Bl5YRgF3RUd1CF1ZVjMhDFBKSkYTGxcWGyFHGxQZR0czZQ'
+// console.log(BniEnc.decrypt(bniresponse['data'], cid, sck))
+
+conn.post(data2)
