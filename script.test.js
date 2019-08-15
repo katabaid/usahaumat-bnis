ca770d796808e07d61b7e820a6a3757e5d06d1be
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2019-08-16 06:15:20 +0700

    Remove unnecessary files

--- script.test.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-const swapi = require('./script')
-
-it('calls swapi to get people', () => {
-  expect.assertions(1)
-  return swapi.getPeople(fetch).then(data => {
-    expect(data.count).toEqual(87)
-  })
-})
